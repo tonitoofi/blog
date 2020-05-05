54c41ef94078670dbb1e8bff7135aba619492939
Antonio Perez <tonypm2708@gmail.com>
2020-05-04 21:21:13 -0400

    creando modulo /about con su routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { PostComponent } from './components/posts/post/post.component';
+import { PostComponent } from "./components/posts/post/post.component";
 
 const routes: Routes = [
-  {path:'', redirectTo:'/home', pathMatch: 'full'},
+  { path: "", redirectTo: "/home", pathMatch: "full" },
   {
     path: "home",
     loadChildren: () =>
       import("./components/pages/home/home.module").then((m) => m.HomeModule),
   },
 
-  {path:'post/:id',component:PostComponent}
+  { path: "post/:id", component: PostComponent },
+  {
+    path: "about",
+    loadChildren: () =>
+      import("./components/pages/about/about.module").then(
+        (m) => m.AboutModule
+      ),
+  },
   // {
   //   path: "posts",
   //   loadChildren: () =>
