aa8a476f87151f75b576298a9041e91893ed79f1
Antonio Perez <tonypm2708@gmail.com>
2020-05-04 21:51:23 -0400

    Estilizando el toolbar

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  public appName:string = 'mYBlog';
+
   constructor() { }
 
   ngOnInit() {
